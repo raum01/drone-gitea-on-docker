815d4b4ff0c676839dbcfca771c30160e4cdcccb
raum01 <raum01@gmail.com>
2021-06-24 13:32:36 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 export HOSTNAME=$(hostname)
-export DRONE_VERSION=1.10.1
-export DRONE_RUNNER_VERSION=1.6.3
-export GITEA_VERSION=1.13
-export IP_ADDRESS=192.168.0.6
+export DRONE_VERSION=latest
+export DRONE_RUNNER_VERSION=latest
+export GITEA_VERSION=latest
+export IP_ADDRESS=$(hostname -i)
 export MINIO_ACCESS_KEY="EXAMPLEKEY"
 export MINIO_SECRET_KEY="EXAMPLESECRET"
-export GITEA_ADMIN_USER="example"
+export GITEA_ADMIN_USER="giteauser"
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
 export DRONE_USER_CREATE="username:${GITEA_ADMIN_USER},machine:false,admin:true,token:${DRONE_RPC_SECRET}"
 export DRONE_GITEA_CLIENT_ID=""
